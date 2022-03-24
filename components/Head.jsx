097e51f1ea381325efee225b6bc5e3cbdb3e7f2c
Brian Lee <blee12@uci.edu>
2022-03-23 22:22:58 -0700

    basic setup of website

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,13 +6,13 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Brian is a software developer that loves diving headfirst into problems and having fun while solving it"
       />
       <meta
         name="keywords"
         content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Brian Lee's Portfolio" />
       <meta
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
@@ -27,5 +27,5 @@ const CustomHead = ({ title }) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Nitin Ranganath',
+  title: 'Brian Lee',
 };
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,21 @@
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
+      <h1>A Little Bit About Me</h1>
+      <h3>I am a software developer at Amazon developing for and maintaining a service that processes and ingests billions of daily device health messages 
+        emitted by millions of Ring devices. The services provide insight into device operating state and sends notifications about system status.
+      </h3>
+      <br></br>
+      <h3>
+        I graduated from University of California - Irvine with a Bachelors of Science in Computer Science back in 2021. There, I graduated with Magna 
+        Cum Laude latin honors with a 3.921 gpa. I also completed two internships while I was attending school: Vusar and Amazon.
+      </h3>
+      <br></br>
+      <h3>
+        As a software developer, I strive to deliver products that have great impact, all while performing beyond expectations. When given a problem, I 
+        look forward to diving in, but only after I understand the scope. I always want to tackle bigger and bigger problems, because that allows me to 
+        improve constantly.
+      </h3>
     </>
   );
 };
