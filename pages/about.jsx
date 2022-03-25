e085782a1ab833050703561b0d0401b585790150
Brian Lee <blee12@uci.edu>
2022-03-25 00:06:05 -0700

    fixing grammatical errors

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,18 +2,18 @@ const AboutPage = () => {
   return (
     <>
       <h1>A Little Bit About Me</h1>
-      <h3>I am a software developer at Amazon developing for and maintaining a service that processes and ingests billions of daily device health messages 
-        emitted by millions of Ring devices. The services provide insight into device operating state and sends notifications about system status.
+      <h3>I am a software developer at Amazon developing and maintaining a service that processes and ingests billions of daily device health messages 
+        emitted by millions of Ring devices. This service provides insight into device operating state and sends notifies users of their device status.
       </h3>
       <br></br>
       <h3>
         I graduated from University of California - Irvine with a Bachelors of Science in Computer Science back in 2021. There, I graduated with Magna 
-        Cum Laude latin honors with a 3.921 gpa. I also completed two internships while I was attending school: Vusar and Amazon.
+        Cum Laude latin honors with a 3.921 GPA. I also completed two internships while I was attending school: Vusar and Amazon.
       </h3>
       <br></br>
       <h3>
         As a software developer, I strive to deliver products that have great impact, all while performing beyond expectations. When given a problem, I 
-        look forward to diving in, but only after I understand the scope. I always want to tackle bigger and bigger problems, because that allows me to 
+        scope out the problem and dive right in. I always want to tackle bigger and bigger problems, because that allows me to 
         improve constantly.
       </h3>
     </>
